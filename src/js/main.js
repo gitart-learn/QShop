d659bd57dda5f1c659f22c253198fd1379c8bfb8
Vladyslav Kuidin <vkuydi@gmail.com>
2022-02-07 19:43:15 +0200

    feat: add posibility open/close cart

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
 import '../styles/main.scss'
 
+const cartBtnEl = document.querySelector('#cartBTN')
+const modalLayerEl = document.querySelector('.modal-overlay')
+const modalCartEl = document.querySelector('#modalCart')
+
+cartBtnEl.addEventListener('click', function () {
+    modalLayerEl.style.display = 'block'
+    modalCartEl.style.display = 'flex'
+
+    document.addEventListener('click', function ({ target }) {
+        if (target.dataset.close || target.classList.contains('modal-overlay')) {
+            modalLayerEl.style.display = ''
+            modalCartEl.style.display = ''
+        }
+    })
+})
