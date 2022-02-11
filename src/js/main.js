9614a8b5cfc159c29a647c872cb7e91deb4fbb65
Vladyslav Kuidin <vkuydi@gmail.com>
2022-02-11 18:27:21 +0200

    refactor and feat: change cart style

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,38 @@
 import '../styles/main.scss'
 
-const cartBtnEl = document.querySelector('#cartBTN')
 const modalLayerEl = document.querySelector('.modal-overlay')
-const modalCartEl = document.querySelector('#modalCart')
-const toCartPageBtnEl = document.querySelector('#toCartPage')
+const modalCartEl = document.querySelector('#modal-cart')
+const toCartPageBtnEl = document.querySelector('#to-cart-page')
+const modalContactsEl = document.querySelector('#modal-contacts')
 
-cartBtnEl.addEventListener('click', function () {
-    modalLayerEl.style.display = 'block'
-    modalCartEl.style.display = 'flex'
-
-    document.addEventListener('click', function ({ target }) {
-        if (target.dataset.close || target.classList.contains('modal-overlay')) {
-            modalLayerEl.style.display = ''
-            modalCartEl.style.display = ''
+document.addEventListener('click', function ({ target }) {
+    if (target.dataset.id === 'cart-btn' || target.id === 'modal-contacts-btn') {
+        modalLayerEl.style.display = 'block'
+        if (target.dataset.id === 'cart-btn') {
+            modalCartEl.style.display = 'flex'
         }
-    })
-    toCartPageBtnEl.addEventListener('click', () => window.open('./index2.html'))
+        else if (target.id === 'modal-contacts-btn') {
+            modalContactsEl.style.display = 'block'
+        }
+
+        document.addEventListener('click', function ({ target }) {
+            if (target.dataset.close || target.classList.contains('modal-overlay')) {
+                modalContactsEl.classList.add('animate__fadeOut')
+                modalCartEl.classList.add('animate__fadeOut')
+                modalLayerEl.classList.add('animate__fadeOut')
+                
+                setTimeout(() => {
+                    modalContactsEl.style.display = ''
+                    modalLayerEl.style.display = ''
+                    modalCartEl.style.display = ''
+                    modalContactsEl.classList.remove('animate__fadeOut')
+                    modalCartEl.classList.remove('animate__fadeOut')
+                    modalLayerEl.classList.remove('animate__fadeOut')
+                }, 700)
+            }
+        })
+    }
+
 })
+toCartPageBtnEl.addEventListener('click', () => { window.open('./index2.html') })
+
